0c515581bd0baad75e4fb9da7c9b19a73788b6ec
Melanie Hoffmann <s0563911@htw-berlin.de>
2019-05-09 14:39:39 +0200

    BE Test With Prisma

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Prisma } from './generated/prisma-client/index';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+
+
+@Module({
+  imports: [ 
+    Prisma,
+    GraphQLModule.forRoot({
+     autoSchemaFile: 'schema.gql',
+    }),
+  
+  
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
